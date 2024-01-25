654924163ae4a08c6f8f15f95f57e2ef0e15c94a
anthrofax <afridhoikhsan@gmail.com>
2024-01-25 13:29:45 +0700

    Still assets lecture, fix image problem (doesn\nt showed) & tidy up about me container

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,5 +1,5 @@
-<div class="container bg-body-tertiary mt-5 p-5">  
+<div class="container bg-body-tertiary mt-5 p-5 d-flex flex-column row-gap-3">  
     <h1>About me</h1>
-    <img src="<?= BASE_URL; ?>/img/myself.jpeg" alt="Gambar Afridho" width="200" class="rounded-circle">
+    <img src="<?= BASE_URL; ?>/img/myself.png" alt="Gambar Afridho" width="200" class="rounded-circle shadow">
     <p>Halo! nama saya <?= $data['name'] ?>, saya seorang <?= $data['job'] ?>, saya berusia <?= $data['age'] ?> tahun.</p>
 </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -9,6 +9,7 @@
 <body>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid px-5">
+    
     <a class="navbar-brand" href="<?= BASE_URL?>">PHP MVC</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
