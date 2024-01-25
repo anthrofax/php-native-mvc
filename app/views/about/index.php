30a9e8836eca8cb418c60d9b5c90e968ce8315dd
anthrofax <afridhoikhsan@gmail.com>
2024-01-25 13:07:17 +0700

    Adding assets in MVC structure

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-<h1>About me</h1>
-
-<p>Halo! nama saya <?=$data['name']?>, saya seorang <?=$data['job']?>, saya berusia <?=$data['age']?> tahun.</p>
+<div class="container bg-body-tertiary mt-5 p-5">  
+    <h1>About me</h1>
+    <img src="<?= BASE_URL; ?>/img/myself.jpeg" alt="Gambar Afridho" width="200" class="rounded-circle">
+    <p>Halo! nama saya <?= $data['name'] ?>, saya seorang <?= $data['job'] ?>, saya berusia <?= $data['age'] ?> tahun.</p>
+</div>
